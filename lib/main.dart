d785409fc8d831e308a5aa58aed3b8e95344c2a3
Darío Rivera <fermius.us@gmail.com>
2021-04-27 19:59:48 -0500

    feat: create flutter app scaffolding

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Title')),
+        body: Center(
+          child: Text('Hello World'),
+        ));
+  }
+}
